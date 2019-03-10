9989ec70457687371e8185cbc39427d61581c64d
erwin.van.den.beld <erwin.van.den.beld@topicus.nl>
2019-03-10 22:15:31 +0100

    Clean code update

--- src/main/resources/static/js/3f.js
+++ src/main/resources/static/js/3f.js
@@ -1,90 +1,34 @@
-var width = window.innerWidth - 100;
-var height = window.innerHeight - 100;
-var currentMainNode;
-var rootNode;
-var link;
-var node;
-var svg;
+var originalNodes;
+var force;
+var links;
+var nodes;
 
-//FIXME linkDistance depending on level
-var force = d3.layout.force()
-    .linkDistance(130)
-    .charge(-450)
-    .gravity(.06)
-    .size([width, height])
-    .on("tick", tick);
+function show3F(jsonPath) {
+    var width = window.innerWidth - 50;
+    var height = window.innerHeight - 50;
+    var svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height);
 
+    createForce(width, height);
 
-function show(d3Svg, jsonPath) {
-    svg = d3Svg;
-    link = svg.selectAll(".link");
-    node = svg.selectAll(".node");
+    links = svg.selectAll(".link");
+    nodes = svg.selectAll(".node");
 
-
-    d3.json(jsonPath, function (error, json) {
-        if (error) throw error;
-
-        rootNode = json;
-        currentMainNode = rootNode;
-
-        update();
-    });
+    readJSONAndUpdate(jsonPath);
 }
 
-function update() {
-
-    var nodes = flatten(currentMainNode);
-    var links = d3.layout.tree().links(nodes);
-
-    // Restart the force layout.
-    force
-        .nodes(nodes)
-        .links(links)
-        .start();
-
-    link = link.data(links, function (d) {
-        return d.target.id;
-    });
-    link.enter().insert("line", ".node")
-        .attr("class", "link");
-    link.exit().remove();
-
-
-    node = node.data(nodes, function (d) {
-        return d.id;
-    });
-
-    var nodeEnter = node.enter().append("g")
-        .attr("class", styleClass)
-        .on("click", selectNode)
-        .on("dblclick", resetNodes)
-        .call(force.drag);
-    nodeEnter.append("circle");
-    nodeEnter.append("text");
-    nodeEnter.append("text");
-
-    node.select("circle")
-        .attr("r", function (d) {
-            return Math.sqrt(500000 / d.level) / 10 || 4.5;
-        }).attr("class", styleClass);
-    node.select("g text:first-of-type")
-        .attr("dy", ".35em")
-        .attr("class", nameStyleClass).text(function (d) {
-        return d.name;
-    });
-    node.select("g text:last-of-type")
-        .attr("dy", ".35em")
-        .attr("y", "20px")
-        .attr("class", descriptionStyleClass).text(function (d) {
-        return d.description;
-    });
-
-    node.exit().remove();
-
+function createForce(width, height) {
+    force = d3.layout.force()
+        .linkDistance(140)
+        .charge(-900)
+        .gravity(.06)
+        .size([width, height])
+        .on("tick", transform);
 }
 
-function tick() {
-    link.attr("x1", function (d) {
+function transform() {
+    links.attr("x1", function (d) {
         return d.source.x;
     })
         .attr("y1", function (d) {
@@ -97,10 +41,86 @@ function tick() {
             return d.target.y;
         });
 
-    node.attr("transform", function (d) {
+    nodes.attr("transform", function (d) {
         return "translate(" + d.x + "," + d.y + ")";
     });
 }
+
+function readJSONAndUpdate(jsonPath) {
+    d3.json(jsonPath, function (error, json) {
+        if (error) throw error;
+
+        originalNodes = json;
+        update(originalNodes);
+    });
+}
+
+function update(rootNode) {
+
+    var nodeModel = flatten(rootNode);
+    var linkModel = d3.layout.tree().links(nodeModel);
+
+    updateForce(linkModel, nodeModel);
+    updateLinks(linkModel);
+    updateNodes(nodeModel);
+
+}
+
+function updateForce(linkModel, nodeModel) {
+    force
+        .nodes(nodeModel)
+        .links(linkModel)
+        .start();
+}
+
+function updateLinks(linkModel) {
+    links = links.data(linkModel, function (d) {
+        return d.target.id;
+    });
+    links.enter().insert("line", ".node")
+        .attr("class", "link");
+    links.exit().remove();
+}
+
+function updateNodes(nodeModel) {
+    nodes = nodes.data(nodeModel, function (d) {
+        return d.id;
+    });
+    createNodeItems();
+    updateNodeStyles();
+
+    nodes.exit().remove();
+}
+
+function createNodeItems() {
+    var nodeEnter = nodes.enter().append("g")
+        .attr("class", styleClass)
+        .on("click", selectNode)
+        .on("dblclick", resetNodes)
+        .call(force.drag);
+    nodeEnter.append("circle");
+    nodeEnter.append("text");
+    nodeEnter.append("text");
+}
+
+function updateNodeStyles() {
+    nodes.select("circle")
+        .attr("r", function (d) {
+            return Math.sqrt(500000 / d.level) / 10 || 4.5;
+        }).attr("class", styleClass);
+    nodes.select("g text:first-of-type")
+        .attr("dy", ".35em")
+        .attr("class", nameStyleClass).text(function (d) {
+        return d.name;
+    });
+    nodes.select("g text:last-of-type")
+        .attr("dy", ".35em")
+        .attr("y", "20px")
+        .attr("class", descriptionStyleClass).text(function (d) {
+        return d.description;
+    });
+}
+
 
 function styleClass(d) {
     return d.okay ? "node okay" : "node failed"
@@ -117,34 +137,33 @@ function descriptionStyleClass(d) {
 function selectNode(d) {
     if (d3.event.defaultPrevented) return;
 
-    currentMainNode = d;
-    update();
+    update(d);
 }
 
 function resetNodes() {
     if (d3.event.defaultPrevented) return;
 
-    currentMainNode = rootNode;
-    update();
+    update(originalNodes);
 }
 
 function flatten(root) {
     var nodes = [], i = 0;
+    recurse(root, 1);
+    return nodes;
 
     function recurse(node, level) {
+
+        //FIXME when fitness function is defined call function for result
 
 
         if (node.children) node.children.forEach(function (theNode) {
             recurse(theNode, level + 1);
         });
 
-        if (!node.id) node.id = ++i;
+        //FIXME when this function is okay and all children are okay -> okay
 
+        if (!node.id) node.id = ++i;
         node.level = level;
         nodes.push(node);
     }
-
-
-    recurse(root, 1);
-    return nodes;
 }
