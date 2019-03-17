3d481ff58455d697cee376355c67a1984a5a8e10
Jan Verhoeckx <jan.verhoeckx@topicus.nl>
2019-03-17 22:48:08 +0100

    - added method to update the data of the D3Fractalizer - added a screenshot to the README

--- src/main/frontend/src/entry.js
+++ src/main/frontend/src/entry.js
@@ -2,4 +2,14 @@ import {D3Fractalizer} from './3f/D3Fractalizer';
 import './style/default.css';
 import data from './examples/webapp.json';
 
-new D3Fractalizer(data);
+let fractalizer = new D3Fractalizer(data);
+
+setTimeout(() => {
+    data.children[1].okay = false;
+    data.children[0].okay = false;
+    data.children[0].children[0].okay = false;
+
+    console.log(data);
+
+    fractalizer.updateWithData(data);
+}, 5000);
